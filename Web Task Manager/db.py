c51afda358f3e920c47981f0e67f799f45733905
Yerson Arango <yersonarango159@gmail.com>
2024-09-10 12:19:11 +0200

    Add files via upload

---
+++ Web Task Manager/db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("sqlite:///database/scores.db",
+                       connect_args={'check_same_thread': False})
+
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
